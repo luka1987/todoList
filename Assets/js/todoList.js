c09dcc4f8fe0dd22a9dae1883ad34523feff3b2f
luka1987 <zdravkovicx5b@gmail.com>
2017-03-05 17:02:33 +0100

    Added all functionalities of icons

--- Assets/js/todoList.js
+++ Assets/js/todoList.js
@@ -1,3 +1,24 @@
-$('li').on('click', function(){
+$('ul').on('click', 'li', function(){
 	$(this).toggleClass('complited');
+});
+
+//click on X to delete Todo
+$('ul').on('click', 'span', function(event){
+	$(this).parent().remove();
+	event.stopPropagation();
+})
+
+$("input[type = 'text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val('');
+		//create a new li and add to ul
+		$('ul').append("<li><span><i class = 'fa fa-trash'></i></span> " + todoText + "</li");
+}
+})
+
+$('.fa-plus').click(function(){
+	$("input[type = 'text']").fadeToggle();
+
 });
